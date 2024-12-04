f504d3e2be41a1dbac2089023ab33e52d5c234f4
Мария Пехова <120147174+mariatle@users.noreply.github.com>
2024-12-04 14:20:28 +0300

    Update lab13.sql

--- lab13.sql
+++ lab13.sql
@@ -1,5 +1,6 @@
 USE master;
 GO
+-- 1) Создать две базы данных на одном экземпляре СУБД SQL Server 2012
 
 IF DB_ID('lab13db1') IS NOT NULL
 	DROP DATABASE lab13db1;
@@ -8,6 +9,7 @@ GO
 IF DB_ID('lab13db2') IS NOT NULL
 	DROP DATABASE lab13db2;
 GO
+
 
 CREATE DATABASE lab13db1
 ON 
@@ -18,7 +20,6 @@ ON
 	MAXSIZE = UNLIMITED, 
 	FILEGROWTH = 5%
 )
-
 GO 
 
 CREATE DATABASE lab13db2 
@@ -30,16 +31,15 @@ ON
 	MAXSIZE = UNLIMITED, 
 	FILEGROWTH = 5%
 );
-
 GO
 
--- Создать в базах данных п.1. горизонтально фрагментированные таблицы для аренды снаряжения.
+-- 2) Создать в базах данных п.1. горизонтально фрагментированные таблицы для аренды снаряжения.
 USE lab13db1
 GO
 
 IF OBJECT_ID(N'Equipment') IS NOT NULL
 	DROP TABLE Equipment;
-go
+GO
 
 CREATE TABLE Equipment
 (
@@ -50,14 +50,14 @@ CREATE TABLE Equipment
 	AvailabilityStatus NVARCHAR(50) NOT NULL,
 	ProductionDate SMALLDATETIME NOT NULL
 );
-go
+GO
 
 USE lab13db2
 GO
 
 IF OBJECT_ID(N'Equipment') IS NOT NULL
 	DROP TABLE Equipment;
-go
+GO
 
 CREATE TABLE Equipment
 (
@@ -68,12 +68,12 @@ CREATE TABLE Equipment
 	AvailabilityStatus NVARCHAR(50) NOT NULL,
 	ProductionDate SMALLDATETIME NOT NULL
 );
-go
+GO
 
--- Создать секционированные представления, обеспечивающие работу с данными таблиц для аренды снаряжения.
+-- 3) Создать секционированные представления, обеспечивающие работу с данными таблиц для аренды снаряжения.
 IF OBJECT_ID(N'EquipmentView') IS NOT NULL
 	DROP VIEW EquipmentView;
-go
+GO
 
 CREATE VIEW EquipmentView AS
 	SELECT * FROM lab13db1.dbo.Equipment
@@ -86,8 +86,24 @@ INSERT INTO EquipmentView (EquipmentID, SerialNumber, Model, RentalPrice, Availa
 (2, 'SN123456789012346', 'Snowboard', 300.00, 'Available', '2021-05-01'),
 (3, 'SN123456789012347', 'Ski Boots', 200.00, 'Rented', '2023-01-01'),
 (4, 'SN123456789012348', 'Helmet', 50.00, 'Available', '2021-11-01'),
-(5, 'SN123456789012349', 'Snowboard Boots', 180.00, 'Rented', '2022-02-01')
-go
+(5, 'SN123456789012349', 'Snowboard', 180.00, 'Rented', '2022-02-01'), 
+(6, 'SN123456789012350', 'Gloves', 30.00, 'Available', '2020-12-15'),
+(7, 'SN123456789012351', 'Jacket', 400.00, 'Rented', '2023-04-10'),
+(8, 'SN123456789012352', 'Helmet', 250.00, 'Available', '2022-06-05'), 
+(9, 'SN123456789012353', 'Ski Goggles', 100.00, 'Available', '2021-03-20'),
+(10, 'SN123456789012354', 'Ski Backpack', 120.00, 'Rented', '2023-02-01'),
+(11, 'SN123456789012355', 'Ski Poles', 500.00, 'Available', '2022-09-01'), 
+(12, 'SN123456789012356', 'Ski Suit', 800.00, 'Rented', '2023-07-10'),
+(13, 'SN123456789012357', 'Ski Gloves', 35.00, 'Available', '2020-10-25'),
+(14, 'SN123456789012358', 'Snowboard Bag', 220.00, 'Rented', '2021-08-01'),
+(15, 'SN123456789012359', 'Helmet', 70.00, 'Available', '2023-03-15'), 
+(16, 'SN123456789012360', 'Boot Warmers', 60.00, 'Available', '2022-01-10'),
+(17, 'SN123456789012361', 'Avalanche Beacon', 1200.00, 'Available', '2023-05-22'),
+(18, 'SN123456789012362', 'Snowshoes', 180.00, 'Available', '2020-11-15'),
+(19, 'SN123456789012363', 'Ski Poles', 160.00, 'Available', '2021-12-01'), 
+(20, 'SN123456789012364', 'Ski Boots', 300.00, 'Rented', '2023-06-12');
+GO
+
 
 SELECT * FROM EquipmentView ORDER BY ProductionDate;
 
@@ -96,7 +112,7 @@ SELECT * FROM lab13db2.dbo.Equipment
 GO
 
 UPDATE EquipmentView
-SET AvailabilityStatus = 'Rented' WHERE Model = 'Snowboard'
+SET AvailabilityStatus = 'Rented' WHERE Model = 'Ski Poles'
 GO
 
 SELECT * FROM lab13db1.dbo.Equipment
