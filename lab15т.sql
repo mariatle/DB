b13551991701fcb39792b6a38b6268832660fa60
Мария Пехова <120147174+mariatle@users.noreply.github.com>
2024-12-28 11:26:02 +0300

    Update lab15т.sql

--- lab15т.sql
+++ lab15т.sql
@@ -1,163 +1,201 @@
 USE lab13db1
-go
+GO
 
-IF OBJECT_ID(N'TypeOfCustomer') IS NOT NULL
-	DROP TABLE TypeOfCustomer;
-go
+IF OBJECT_ID(N'EquipmentType') IS NOT NULL
+	DROP TABLE EquipmentType;
+GO
 
-CREATE TABLE TypeOfCustomer(
+CREATE TABLE EquipmentType(
 	Name NVarChar(100) PRIMARY KEY NOT NULL,
-	Description NVarChar(500),
+	Description NVarChar(500)
 );
+GO
 
 USE lab13db2
-go
+GO
 
-IF OBJECT_ID(N'Customer') IS NOT NULL
-	DROP TABLE Customer;
-go
+IF OBJECT_ID(N'EquipmentRental') IS NOT NULL
+	DROP TABLE EquipmentRental;
+GO
 
-CREATE TABLE Customer(
-	ID INT IDENTITY(1,1) PRIMARY KEY  NOT NULL, 
+CREATE TABLE EquipmentRental(
+	ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
 	Name NVarChar(100) NOT NULL,
 	StartTime SmallDateTime NOT NULL DEFAULT GETDATE(),
 	EndTime SmallDateTime NULL,
-	Coef FLOAT NULL,
-	HoursOfRent FLOAT NULL,
+	HoursRented FLOAT NULL
 );
-go
-
--- 2. Создать необходимые элементы базы данных (представления, триггеры), обеспечивающие работу
--- с данными связанных таблиц (выборку, вставку, изменение, удаление).
-
-IF OBJECT_ID(N'CustomerView') IS NOT NULL
-	DROP VIEW CustomerView;
-go
-
-CREATE VIEW CustomerView AS
-	SELECT f.ID, f.Name, s.Description, f.StartTime, f.EndTime, f.Coef, f.HoursOfRent FROM Customer as f INNER JOIN lab13db1.dbo.TypeOfCustomer as s ON f.Name = s.Name
-go
-
-USE lab13db1
-go
-
-IF OBJECT_ID(N'Type_delete_trg') IS NOT NULL
-	DROP TRIGGER Type_delete_trg;
-go
-
-IF OBJECT_ID(N'Type_update_trg') IS NOT NULL
-	DROP TRIGGER Type_update_trg;
-go
-
-
-CREATE TRIGGER Type_delete_trg ON TypeOfCustomer
-FOR DELETE AS
-	DELETE Customer FROM lab13db2.dbo.Customer AS Customer
-		INNER JOIN deleted ON Customer.Name= deleted.Name
 GO
 
-CREATE TRIGGER Type_update_trg ON TypeOfCustomer
-FOR UPDATE AS 
+-- Создание представления
+IF OBJECT_ID(N'RentalView') IS NOT NULL
+	DROP VIEW RentalView;
+GO
+
+CREATE VIEW RentalView AS
+	SELECT r.ID, r.Name, e.Description, r.StartTime, r.EndTime, r.HoursRented 
+	FROM EquipmentRental AS r 
+	INNER JOIN lab13db1.dbo.EquipmentType AS e ON r.Name = e.Name;
+GO
+
+select * from RentalView
+
+USE lab13db1
+GO
+
+-- Создание триггеров для таблицы EquipmentType
+IF OBJECT_ID(N'EquipmentType_delete_trg') IS NOT NULL
+	DROP TRIGGER EquipmentType_delete_trg;
+GO
+
+IF OBJECT_ID(N'EquipmentType_update_trg') IS NOT NULL
+	DROP TRIGGER EquipmentType_update_trg;
+GO
+
+
+-- срабатывает при удалении записи из EquipmentType, удаляет все записи в таблице EquipmentRental где тип совп с удаляемым
+CREATE TRIGGER EquipmentType_delete_trg ON EquipmentType
+FOR DELETE AS
+	DELETE rental 
+	FROM lab13db2.dbo.EquipmentRental AS rental
+	INNER JOIN deleted ON rental.Name = deleted.Name;
+GO
+
+CREATE TRIGGER EquipmentType_update_trg ON EquipmentType
+FOR UPDATE AS
 	IF UPDATE(Name)
 	BEGIN
-        RAISERROR('Нельзя менять название у созданного типа работы', 16, 1);
+        RAISERROR('Название типа нельзя изменить!!!!!!!!!', 16, 1);
 		ROLLBACK;
-    END
-go
+    END;
+GO
 
 USE lab13db2
-go
+GO
+
+-- Создание триггеров для таблицы EquipmentRental
+IF OBJECT_ID(N'EquipmentRental_insert_trg') IS NOT NULL
+	DROP TRIGGER EquipmentRental_insert_trg;
+GO
+
+IF OBJECT_ID(N'EquipmentRental_update_trg') IS NOT NULL
+	DROP TRIGGER EquipmentRental_update_trg;
+GO
 
 
-IF OBJECT_ID(N'Customer_insert_trg') IS NOT NULL
-	DROP TRIGGER Customer_insert_trg;
-go
-
-IF OBJECT_ID(N'Customer_update_trg') IS NOT NULL
-	DROP TRIGGER Customer_update_trg;
-go
-
-
-CREATE TRIGGER Customer_update_trg ON Customer
-FOR UPDATE AS 
-	IF UPDATE(Name) AND 
-		EXISTS (SELECT 1 FROM lab13db1.dbo.TypeOfCustomer as type RIGHT JOIN inserted ON inserted.Name = type.Name WHERE type.Name IS NULL) -- фикс
-	BEGIN
-        RAISERROR('При обновлении необходимо выбрать существующий тип работы', 16, 1);
-		ROLLBACK;
-    END
-go
-
-CREATE TRIGGER Customer_insert_trg ON Customer
+-- чек сущ-т ли указанный тип оборудования в таблице EquipmentType, если такого типа нет, то вставка откатывается.
+CREATE TRIGGER EquipmentRental_insert_trg ON EquipmentRental
 FOR INSERT AS
-	IF EXISTS (SELECT 1 FROM lab13db1.dbo.TypeOfCustomer as type RIGHT JOIN inserted ON inserted.Name = type.Name WHERE type.Name IS NULL) -- фикс
+	IF EXISTS (SELECT 1 FROM lab13db1.dbo.EquipmentType AS type 
+		RIGHT JOIN inserted ON inserted.Name = type.Name 
+		WHERE type.Name IS NULL
+	)
 	BEGIN
-		RAISERROR('При вставке необходимо выбрать существующий тип работы', 16, 1);
+		RAISERROR('При добавлении необходимо указать существующий тип оборудования.', 16, 1);
 		ROLLBACK;
-	END
+	END;
+GO
+
+-- если апд тип оборудования и он не сущ-т в таблице EquipmentType, операция откатывается
+CREATE TRIGGER EquipmentRental_update_trg ON EquipmentRental
+FOR UPDATE AS
+	IF UPDATE(Name) AND EXISTS ( -- чек, была ли изменена колонка Name в таблице EquipmentRental, если апд, то true
+			SELECT 1 
+			FROM lab13db1.dbo.EquipmentType AS type 
+			RIGHT JOIN inserted ON inserted.Name = type.Name   --все записи из inserted, для которых нет соответствующего типа оборудования в EquipmentType
+			WHERE type.Name IS NULL
+		)
+	BEGIN
+        RAISERROR('Обновление возможно только с существующим типом оборудования.', 16, 1);
+		ROLLBACK;
+    END;
 GO
 
 USE lab13db1
-go
-
-INSERT INTO TypeOfCustomer VALUES
-(N'Прокат лыж', N'Аренда комплекта лыж для катания по трассе'),
-(N'Прокат сноуборда', N'Замена масла на масло Castrol'),
-(N'Прокат горнолыжных ботинок', N'Аренда ботинок для лыжных комплексов')
-go
+GO
 
 
-SELECT * FROM TypeOfCustomer;
-go
+USE lab13db1
+GO
+
+INSERT INTO EquipmentType (Name, Description) VALUES
+(N'Горные лыжи', N'Комплект горных лыж с палками'),
+(N'Сноуборд', N'Комплект сноуборда с креплениями и ботинками'),
+(N'Шлем', N'Защитный шлем для зимних видов спорта'),
+(N'Лыжные палки', N'Комплект лыжных палок для горных лыж'),
+(N'Сноубордические ботинки', N'Комплект ботинок для сноуборда');
+GO
+
+
+SELECT * FROM EquipmentType;
+GO
 
 USE lab13db2
-go
+GO
 
-INSERT INTO Customer (Name, Coef, HoursOfRent) VALUES
-(N'Прокат лыж', 1, 2.0),
-(N'Прокат сноуборда', 2, 2.0);
-go
+INSERT INTO EquipmentRental (Name, HoursRented) VALUES
+(N'Горные лыжи', 4.0),
+(N'Сноуборд', 3.0),
+(N'Шлем', 2.0),
+(N'Лыжные палки', 5.0),
+(N'Сноубордические ботинки', 6.0);
+GO
 
-SELECT * FROM CustomerView;
-go
+SELECT * FROM RentalView;
+GO
 
---проверка триггеров TypeOfCustomer
-UPDATE lab13db1.dbo.TypeOfCustomer SET Name = N'Обновлённое название' WHERE Name = N'Прокат лыж';
-go
 
-UPDATE lab13db1.dbo.TypeOfCustomer SET Description = N'Обновлённое описание' WHERE Name = N'Прокат лыж';
-go
 
-SELECT * FROM CustomerView;
-go
+UPDATE lab13db1.dbo.EquipmentType 
+SET Name = N'Обновлённое название' 
+WHERE Name = N'Горные лыжи';
+GO
 
-DELETE FROM lab13db1.dbo.TypeOfCustomer WHERE Name = N'Прокат лыж';
-go
+UPDATE lab13db1.dbo.EquipmentType 
+SET Description = N'Обновлённое описание' 
+WHERE Name = N'Горные лыжи';
+GO
 
-SELECT * FROM CustomerView;
-go
+DELETE FROM lab13db1.dbo.EquipmentType 
+WHERE Name = N'Горные лыжи';
+GO
 
---проверка триггеров Customer
-INSERT INTO Customer (Name, Coef, HoursOfRent) VALUES
-(N'Несуществующее название', 1, 2.0),
-(N'Прокат горнолыжных ботинок', 1, 2.0);
-go -- это ломает
+SELECT * FROM RentalView;
+GO
 
-SELECT * FROM Customer;
-go
+-- Проверка триггеров EquipmentRental
+INSERT INTO EquipmentRental (Name, HoursRented) VALUES
+(N'Кринж название', 2.0),
+(N'Шлем', 1.0);
+GO
 
-UPDATE Customer SET Name = N'Прокат сноуборда' WHERE Name = N'Прокат горнолыжных ботинок';
-go
+SELECT * FROM EquipmentRental;
+GO
 
-SELECT * FROM lab13db1.dbo.TypeOfCustomer;
-go
+UPDATE EquipmentRental 
+SET Name = N'Шлем' 
+WHERE Name = N'Сноуборд';
+GO
 
-SELECT * FROM CustomerView;
-go
+SELECT * FROM lab13db1.dbo.EquipmentType;
+GO
 
-UPDATE Customer SET Name = N'Прокат горнолыжных ботинок' WHERE ID = 2;
-go
+SELECT * FROM RentalView;
+GO
 
-SELECT * FROM CustomerView;
-go
+UPDATE EquipmentRental 
+SET Name = N'Амням' 
+WHERE ID = 2;
+GO
 
+select * from EquipmentRental
+SELECT * FROM lab13db1.dbo.EquipmentType;
+
+
+UPDATE EquipmentRental 
+SET Name = N'Шлем' 
+WHERE ID = 2;
+GO
+
+SELECT * FROM RentalView;
+GO
